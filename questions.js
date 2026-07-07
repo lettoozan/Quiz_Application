1560cd30752b079c0a1f7bcf67589d22c9603482
lettoozan <lettoozan08@gmail.com>
2026-07-07 14:31:33 +0300

    test2

--- questions.js
+++ questions.js
@@ -13,5 +13,5 @@ let questions = [
     new Question("2-Hangisi frontend kapsamında değerlendirilmez?", {a: ": Css", b: ": HTML", c: ": JavaScript", d: ": SQL"}, "d"),
     new Question("3-Hangisi backend kapsamında değerlendirilir?", {a: ": Node.Js", b: ": Typescript", c: ": Angular", d: ": React"}, "a"),
     new Question("4-Hangisi JavaScript programlama dilini kullanmaz?", {a: ": React", b: ": Angular", c: ": Vuejs", d: "Asp.net"}, "d"),
-    new Question("5-Test?", {a: ": test", b: ": test", c: ": test", d: "a"}, "a")
+    new Question("5-Test?", {a: ": test", b: ": test", c: ": test", d: "test"}, "a")
 ];
